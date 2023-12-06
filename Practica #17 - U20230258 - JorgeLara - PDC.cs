93bde1540fbe5ffebad56e0794fd3c2e5b2a49ce
Your Name <you@example.com>
2023-12-06 13:06:34 -0600

    Practica #17 - U20230258 - JorgeLara - PDC

---
+++ Practica #17 - U20230258 - JorgeLara - PDC.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            int opciones;
+
+            do
+            {
+
+                Console.WriteLine("\nMenú");
+                Console.WriteLine("1. Buscar y Mostrar el contenido de un archivo");
+                Console.WriteLine("2. Salir.");
+                Console.Write("\nSeleccione una opcion entre (1-2): ");
+
+                opciones = Convert.ToInt32(Console.ReadLine());
+
+                    switch (opciones)
+                    {
+
+                        case 1:
+
+                            ShowfileContent();
+                            break;
+                        case 2:
+
+                            Console.WriteLine("\nSaliendo del programa....");
+                            break;
+                        default:
+
+                            Console.WriteLine("Opción no válida, intente de nuevo.");
+                            break;
+                    }
+
+            } while (opciones != 2);
+        }
+
+        static void ShowfileContent()
+        {
+            Console.Write("\nIngrese la ruta del archivo: ");
+            string? filePath = Console.ReadLine();
+
+            if (File.Exists(filePath))
+            {
+                string content = ReadFileContent(filePath);
+                Console.WriteLine($"\nContenido del archivo:\n{content}");
+            }
+            else
+            {
+                Console.WriteLine("El archivo no existe en la ruta proporcionada.");
+            }
+        }
+
+        static string ReadFileContent(string filePath)
+        {
+            try
+            {
+                // Leer todo el contenido del archivo
+                string content = File.ReadAllText(filePath);
+                return content;
+            }
+            catch (Exception ex)
+            {
+                return $"Error al leer el archivo: {ex.Message}";
+            }
+        }
+    }
+}
